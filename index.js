f93cd2b335123abd163231841ee09b0b3861138a
hanse <bluewood.truth@gmail.com>
2023-02-17 20:33:25 +0900

    feat: implement replaceKeys function

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello world');
+const mapObject = require('map-obj');
+
+function replaceKeys(target, replaceKeyMap, options = { deep: true }) {
+  return mapObject(
+    target,
+    function (key, value) {
+      let replaced = replaceKeyMap[key];
+
+      return [
+        replaced === undefined
+          ? key
+          : typeof replaced === 'function'
+          ? replaced(key)
+          : replaced,
+        value,
+      ];
+    },
+    options
+  );
+}
+
+modules.exports = replaceKeys;
